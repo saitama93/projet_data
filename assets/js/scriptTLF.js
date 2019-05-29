5a4af2deb0608b9f4a47b99a8f3dc0fffcd0081d
mgaaintjrae <pr.priscilla.roy@gmail.com>
2019-05-29 11:02:01 +0200

    page accueil/autour de moi/tous les festivals/contact OK reste la navbar

--- assets/js/scriptTLF.js
+++ assets/js/scriptTLF.js
@@ -2,30 +2,31 @@
 var open = document.querySelector('.recherche');
 var close = document.querySelector('.closebtn');
 
+var openLoopPC = document.querySelector('.loupe');
+
 //EVENEMENTS
-open.addEventListener ('click',openRecherche);
-close.addEventListener ('click',closeRecherche);
+openLoopPC.addEventListener('click', loupeOpen);
+
+open.addEventListener('click', openRecherche);
+close.addEventListener('click', closeRecherche);
+
 // fenêtre qui s'affiche quand on click sur la loupe  
-        document.getElementById("sideRecherche").style.display = "none";
-    function openRecherche() {   
-            
-        document.getElementById("sideRecherche").style.top = "0";
-        document.getElementById("sideRecherche").style.display = "block"; 
-        document.getElementById("mainTLF").style.maringTop = "150px";        
-    }
+document.getElementById("sideRecherche").style.display = "none";
 
-    function closeRecherche() {
-        document.getElementById("sideRecherche").style.top = "-300px";
-        document.getElementById("mainTLF").style.marginTop = "0";        
-    }  
+function openRecherche() {
 
+    document.getElementById("sideRecherche").style.top = "0";
+    document.getElementById("sideRecherche").style.display = "block";
+    document.getElementById("mainTLF").style.maringTop = "150px";
+}
 
+function closeRecherche() {
+    document.getElementById("sideRecherche").style.top = "-300px";
+    document.getElementById("mainTLF").style.marginTop = "0";
+}
 
-
-
-
-    // fenêtre qui s'affiche quand on click sur "par mois"
-    // document.getElementById("searchMois").style.display = "none";
-    // function openSearchMois(){
-        
-    // }
+function loupeOpen(){
+    document.getElementById("sideRecherche").style.top = "0";
+    document.getElementById("sideRecherche").style.display = "block";
+    document.getElementById("mainTLF").style.maringTop = "150px";
+}
